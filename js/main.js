f9daedb214a7b39b55aae84ef215c4cf06901a52
lex <project.phoenix.scp@gmail.com>
2013-11-23 04:50:40 +1100

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+$('textarea').blur(function() {
+	var txt=$('#notes').val();
+	localStorage.notes=txt.escapeHTML();
+	console.log(txt);
+});
+$(function() {
+	if(localStorage.notes){
+		$('#notes').val(localStorage.notes.unescapeHTML());
+	}
+});
 var canvasElement = document.getElementById('mainCanvas');
 sheetengine.scene.init(canvasElement, {w:canvasElement.width,h:canvasElement.height});
-
 var offset=-315;
 var colours=['#FF652C','#FFE62C','#282828','#4083FF','#FFB52C'];
 var floor=[];
@@ -10,7 +19,7 @@ function getMousePos(evt) {
 	var rect = canvasElement.getBoundingClientRect();
 	return {
 		x: evt.clientX - rect.left,
-			y: evt.clientY - rect.top
+		y: evt.clientY - rect.top
 	};
 }
 //--------------Conversions (chainable)
@@ -53,9 +62,9 @@ function changeSquare(square,colour){
 function isObjectHovered(puv, obj) {
 	var ouv = sheetengine.drawing.getPointuv(obj.centerp);
 	if (puv.u > ouv.u - 32 &&
-			puv.u < ouv.u - 22 + 44 &&
-			puv.v > ouv.v - 22 &&
-			puv.v < ouv.v - 22 + 68)
+	    puv.u < ouv.u - 22 + 44 &&
+		    puv.v > ouv.v - 22 &&
+			    puv.v < ouv.v - 22 + 68)
 		return true;
 	return false;
 }
@@ -106,34 +115,34 @@ function drawFloor(){
 function arch(pos){
 	this.sheet = new sheetengine.Sheet({
 		x: 0,
-	      y: 0,
-	      z: 0
+		y: 0,
+		z: 0
 	}, {
 		alphaD: 0,
-	      betaD: 0,
-	      gammaD: 90
+		betaD: 0,
+		gammaD: 90
 	}, {
 		w: 200,
-	      h: 80
+		h: 80
 	});
 	this.obj = new sheetengine.SheetObject({
 		x: pos.x,
-	    y: pos.y,
-	    z: +55
+		y: pos.y,
+		z: +55
 	}, {
 		alphaD: 0,
-	    betaD: 0,
-	    gammaD: 0
+		betaD: 0,
+		gammaD: 0
 	}, [this.sheet], {
 		w: 200,
-	    h: 80,
-	    relu: 20,
-	    relv: 50
+		h: 80,
+		relu: 20,
+		relv: 50
 	});
 	this.obj.setShadows(false, false);
-this.sheet.context.fillStyle = '#12FF00';
-this.sheet.context.fillRect(0, 0, 200, 80);
-_update();
+	this.sheet.context.fillStyle = '#12FF00';
+	this.sheet.context.fillRect(0, 0, 200, 80);
+	_update();
 
 }
 function menuHandler(){
@@ -144,7 +153,7 @@ if(localStorage.stats){
 	loadStats();
 	menuHandler();
 }else{
-var stats={movement:0};
+	var stats={movement:0};
 }
 function saveStats(){
 	localStorage.stats=JSON.stringify(stats);
@@ -154,18 +163,29 @@ function loadStats(){
 }
 var xm=[-1,0,1,0];
 var ym=[0,-1,0,1];
+function displayCoords(){
+	for(var i=0;i<things.length;i++){
+		console.log(things[i])	
+	}
+}
 //-----------------SCP 1384 object;
 function makeSCP(pos){
 	var that=this;
+	this.type='scp';
 	this.move=function(dir){
 		//Accessable via scp.move
 		var oldCoords=this.coords;
+		things[oldCoords.x][oldCoords.y]=0;
 		var newCoords={x:oldCoords.x+xm[dir],y:oldCoords.y+ym[dir]}
+		things[newCoords.x][newCoords.y]=this;
 		actuallyMove(newCoords);
 	}
 	function actuallyMove(coords){
-//		console.log(colours[floor[coords.x-1][coords.y-1]]);
-		floor[coords.x][coords.y].color='green';
+		//don't move if square is yellow
+		if(floor[coords.x+1][coords.y+1]==1){
+			console.log('cannot move here')
+			return;
+		}
 		//ANIMATION GOES HERE
 		that.obj.setPosition(matrix[coords.x][coords.y].centerp);
 		stats.movement+=1;
@@ -180,34 +200,100 @@ function makeSCP(pos){
 	}
 	this.sheet = new sheetengine.Sheet({
 		x: 0,
-	      y: 0,
-	      z: 0
+		y: 0,
+		z: 0
 	}, {
 		alphaD: 0,
-	      betaD: 0,
-	      gammaD: 45
+		betaD: 0,
+		gammaD: 45
 	}, {
 		w: 30,
-	      h: 64
+		h: 64
 	});
 	this.obj = new sheetengine.SheetObject({
 		x: pos.x,
-	    y: pos.y,
-	    z: 0
+		y: pos.y,
+		z: 0
 	}, {
 		alphaD: 0,
-	    betaD: 0,
-	    gammaD: 0
+		betaD: 0,
+		gammaD: 0
 	}, [this.sheet], {
 		w: 100,
-	    h: 64,
-	    relu: 20,
-	    relv: 50
+		h: 64,
+		relu: 20,
+		relv: 50
 	});
 	this.obj.setShadows(false, false);
-this.sheet.context.fillStyle = '#12FF00';
-this.sheet.context.fillRect(0, 0, 30, 64);
-_update();
+	this.sheet.context.fillStyle = '#12FF00';
+	this.sheet.context.fillRect(0, 0, 30, 64);
+	_update();
+
+}
+function researcher(cor){
+	this.type='res';
+	things[cor.x][cor.y]=this;
+	this.coords=cor;
+	var pos=gridToCoords(cor)
+	var that=this;
+	this.move=function(dir){
+		//Accessable via scp.move
+		var oldCoords=this.coords;
+		var newCoords={x:oldCoords.x+xm[dir],y:oldCoords.y+ym[dir]}
+		console.log(oldCoords,newCoords);
+		things[oldCoords.x][oldCoords.y]=0;
+		if(things[newCoords.x][newCoords.y]!==0){
+			if(things[newCoords.x][newCoords.y].type=='scp')
+				console.log('SCP');
+			else if(things[newCoords.x][newCoords.y].type=='res')
+				console.log('Researcher');
+			else
+				console.log('occupied');
+			return;
+		}
+		things[newCoords.x][newCoords.y]=this;
+		actuallyMove(newCoords);
+	}
+	function actuallyMove(coords){
+
+		//ANIMATION GOES HERE
+		that.obj.setPosition(matrix[coords.x][coords.y].centerp);
+		that.coords=coords;
+		_update();
+	}
+	function endCheck(){
+		//Check to see if over the end tiles;
+	}
+	this.sheet = new sheetengine.Sheet({
+		x: 0,
+		y: 0,
+		z: 0
+	}, {
+		alphaD: 0,
+		betaD: 0,
+		gammaD: 45
+	}, {
+		w: 30,
+		h: 64
+	});
+	this.obj = new sheetengine.SheetObject({
+		x: pos.x,
+		y: pos.y,
+		z: 0
+	}, {
+		alphaD: 0,
+		betaD: 0,
+		gammaD: 0
+	}, [this.sheet], {
+		w: 100,
+		h: 64,
+		relu: 20,
+		relv: 50
+	});
+	this.obj.setShadows(false, false);
+	this.sheet.context.fillStyle = 'red';
+	this.sheet.context.fillRect(0, 0, 30, 64);
+	_update();
 
 }
 //----------drawSCP
@@ -235,6 +321,12 @@ function randomLoc(){
 	var y=Number.random(0,maxY);
 	return {obj:matrix[x][y],x:x,y:y};
 }
+var res=[];
+function makeRes(){
+	var x=parseInt($('#x').val());
+	var y=parseInt($('#y').val());
+	res[res.length] = new researcher({x:x,y:y}); 
+}
 //--------shorthand for updating canvas
 function _update(){
 	// draw the scene
@@ -245,15 +337,15 @@ _update();
 //---------------Creating the cursor
 _curs = new sheetengine.Sheet({
 	x: 0,
-      y: 0,
-      z: 0
+	y: 0,
+	z: 0
 }, {
 	alphaD: 90,
-      betaD: 0,
-      gammaD:0 
+	betaD: 0,
+	gammaD:0 
 }, {
 	w: 45,
-      h: 45
+	h: 45
 });
 var entrance=matrix[0][Math.floor(matrix[0].length/2)];
 var entrance2=matrix[0][Math.floor(matrix[0].length/2)-1];
