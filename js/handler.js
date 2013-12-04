a3c79268ca3265dc79e4938c1b0a54df1c620131
lex <project.phoenix.scp@gmail.com>
2013-12-04 16:31:50 +1100

    tidying up

--- js/handler.js
+++ js/handler.js
@@ -1,5 +1,6 @@
+// type #debug and reload for debugging menu
 var debug=(window.location.hash=='#debug'?1:0);
-var first = ((!localStorage.player)||(localStorage.player=="0,12"&&localStorage.scp=="14,6")?1:0);
+var first = ((!localStorage.player)||(localStorage.player=="0,12"&&localStorage.scp=="14,6")?1:0); // set 'first' flag if the player's position is undefined or the starting coordinates
 var a=0;
 window.onload = function(e){ 
 	a=1;
@@ -15,8 +16,6 @@ window.onload = function(e){
 	updateStats();
 
 }
-
-//var count=0;
 function rollover(text,clear){
 	var subj=document.getElementById('tooltip');
 	subj.innerHTML=text;
@@ -36,16 +35,13 @@ function rollover(text,clear){
 }
 var fsText='Go full screen';
 function fullScreen(){
-
 	if (fsText=='Go full screen'){
 		fsText='Exit full screen';
 		document.getElementById('fs').src='img/small.png';
 		document.getElementById('switch2').checked=true;
-		//document.getElementById('Bplus').innerHTML='-'
 	}else{
 		document.getElementById('switch2').checked=false
 		document.getElementById('fs').src='img/full.png';
-		//document.getElementById('Bplus').innerHTML='+'
 		fsText='Go full screen';
 	}
 	var
@@ -59,12 +55,9 @@ function fullScreen(){
 		, cfs = 
 	document.exitFullscreen
 		|| document.mozCancelFullScreen
-			|| document.webkitCancelFullScreen
-			;
-			if(is)
-				cfs.call(document);
-			else
-				rfs.call(el);
+			|| document.webkitCancelFullScreen;
+			if(is)  cfs.call(document);
+			else    rfs.call(el);
 }
 function updateStats(){
 	if(!a) return;
@@ -79,15 +72,12 @@ function updateStats(){
 		}
 	};
 	stats.innerHTML="SCP: ["+stat.scp.x+", "+stat.scp.y+"]<br>Player: ["+stat.me.x+", "+stat.me.y+"]";
-	console.log(stat);
-
 }
-function saveText(){
-
+function saveText(){ // save the notes
 	rollover('Text saved',20);
 	localStorage.notes=document.getElementById('notes').value;
 }
-function dim() {
+function dim() { //calls a big semi-transparent box to dim the screen
 	var e = document.getElementById('dimmer');
 	if(e.style.display !== 'none'){
 		e.style.display = 'none';
@@ -98,8 +88,8 @@ function dim() {
 		e.style.zIndex = '10';
 	}
 }
-function notecheckbox(){
-document.getElementById('notes').style.display='none';
+function notecheckbox(){ 
+	document.getElementById('notes').style.display='none';
 }
 	var fade;
 function toggle_visibility(id) {
@@ -111,7 +101,6 @@ function toggle_visibility(id) {
 				e.style.opacity-=0.1;
 			}else{
 				clearInterval(fade);
-				console.log('aaaa')
 				e.style.display = 'none';
 			}
 		},50);
@@ -127,7 +116,7 @@ function modal(){
 	toggle_visibility('modal');
 	dim();
 }
-function newGame(auto){
+function newGame(auto){//start a new game. auto flag determines conformation box
 	if(!auto){
 		var check=confirm("Really start a new Game?");
 		if(!check){
@@ -137,15 +126,15 @@ function newGame(auto){
 	localStorage.clear();
 	location.reload();
 }
-function getFloor(){
-	if(localStorage.floor){
+function getFloor(){ 
+	if(localStorage.floor){ // if it exists, parse the JSON colours from localstorage and run drawfloor()
 		fl2=JSON.parse(localStorage.floor);
 		for (var d=0;d<Object.keys(fl2).length;d++){
 			floor[d]=fl2[d];
 		}
 		drawFloor();
 
-	}else{
+	}else{ // if the string doesn't exist, make one up then run drawfloor() (also, save it for next time)
 		var subj;
 		for (var x=0; x<=14; x++) {
 			floor[x]=[];     
@@ -158,6 +147,7 @@ function getFloor(){
 		drawFloor();
 	}
 }
+//save the colours of the floor in a JSON string
 function saveFloor(){
 	var fl={};
 	for(var d=0;d<floor.length;d++){
@@ -166,7 +156,9 @@ function saveFloor(){
 	localStorage.floor=JSON.stringify(fl);
 }
 function drawFloor(){
+	//save the size of the floor for doing the side bits
 	var end={x:floor.length-1,y:floor[0].length-1};
+	//arrays for the side bits
 	var sideX=[];
 	var sideY=[];
 	for(var x=0; x<floor.length;x++){
@@ -176,17 +168,16 @@ function drawFloor(){
 		for(var y=0; y<subj.length;y++){
 			things[x][y]=0;
 			matrix[x][y]=basesheet = new sheetengine.BaseSheet({x:offset+x*45,y:offset+y*45,z:0}, {alphaD:90,betaD:0,gammaD:0}, {w:45,h:45});
-			//matrix[x][y].color = floor[x][y];
 			matrix[x][y].color = colours[subj[y]];
 			matrix[x][y].dim = {x:x,y:y};
 			if(x==end.x){
 				sideX[y]= new sheetengine.BaseSheet({x:offset-8+(x+1)*45,y:offset+15+y*45,z:0}, {alphaD:90,betaD:0,gammaD:90}, {w:20,h:45});
-			sideX[y].color = colours2[subj[y]];
+				sideX[y].color = colours2[subj[y]];
 			}
 			if(y==end.y){
 				sideY[x]= new sheetengine.BaseSheet({x:offset+14+(x)*45,y:offset-8+(y+1)*45,z:0}, {alphaD:0,betaD:90,gammaD:0}, {w:20,h:45});
 
-			sideY[x].color = colours3[subj[y]];
+				sideY[x].color = colours3[subj[y]];
 			}
 		}
 	}
@@ -197,14 +188,17 @@ function _update(){ // draw the scene
 	sheetengine.calc.calculateAllSheets();
 	sheetengine.drawing.drawScene(true);
 }
+//changes a square colour both physically and in localStorage
 function changeSquare(square,colour){
-	console.log("changing square from "+colours[floor[square.x][square.y]]+" to "+colours[colour]);
 	floor[square.x][square.y]=colour;
 	saveFloor();
 	matrix[square.x][square.y].color=colours[colour];
 	_update();
 }
+//key handlers
 document.onkeydown = checkKey;
+document.onkeyup = ent;
+// movement keys
 function checkKey(e) {
 	e = e || window.event;
 	if (e.keyCode == '37') {//left
@@ -222,15 +216,15 @@ function checkKey(e) {
 	}
 	return false;
 }
-   function (event) {
-        if (event.which == 13 || event.keyCode == 13) {
-
+//Close open modal with esc or enter
+function ent(event) {
+	if ((event.which == 13 || event.keyCode == 13)||(event.keyCode == 27 || event.which == 27)) {
 		if(document.getElementById('modal').style.display !== 'none'){
 			modal();
 		}else{
 			return true;
 		}
-            return false;
-        }
-        return true;
-    });
+		return false;
+	}
+	return true;
+};
